bd36d6009bffa46715566d6ddb138b3a5b36349a
Jeff Butler <jeffgbutler@gmail.com>
2021-07-09 14:17:19 -0400

    Externalize the login page

--- src/main/java/com/example/radiologyclient/config/SecurityConfiguration.java
+++ src/main/java/com/example/radiologyclient/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,12 +12,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests(a -> a
-                    .antMatchers("/", "/app.js", "/user", "/error", "/login", "/webjars/**").permitAll()
+                    .antMatchers("/openemr_login.html", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
             )
-            .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-            .logout(l -> l.logoutSuccessUrl("/").permitAll())
-            .oauth2Login();
+//            .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+//            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+//            .logout(l -> l.logoutSuccessUrl("/").permitAll())
+            .oauth2Login()
+            .loginPage("/openemr_login.html");
     }
 }
